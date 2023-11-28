bd58b90430efba46479b6eff72e45e94d443c46d
Anthony Quishpe <aequishpe2@espe.edu.ec>
2023-11-28 15:31:55 -0500

    ok
    ok

---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js
+
+// Uso de los componentes simulados
+const headerComponent = new Header('Encabezado','Mi Composicion de Componentes');
+const listComponent = new CustomList(['Lista','Elemento 1', 'Elemento 2', 'Elemento 3']);
+const formComponent = new CustomForm([{ type: 'text', label: 'Nombre' }, { type: 'email', label: 'Correo' }]);
